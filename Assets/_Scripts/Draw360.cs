9e4d97b176e9849ef60e80e7ac03dd0bbc2dad02
goricher <307363020@qq.com>
2019-04-14 00:24:01 +0800

    draw 360

---
+++ Assets/_Scripts/Draw360.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public class Draw360 : MonoBehaviour
+{
+
+    private LineRenderer line;
+
+    public int lineCounts = 360;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = lineCounts * 2;
+
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < lineCounts; ++i)
+        {
+            float angle = i * 360.0f / lineCounts;
+            float radius = angle / 180.0f * PI;
+            Vector3 direction = new Vector3(Cos(radius), Sin(radius), 0.0f);
+
+            Ray ray = new Ray(this.transform.position, direction);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+
+            int min_index = 0;
+            float min_len = Mathf.Infinity;
+
+            for (int index = 0; index < hits.Length; ++index)
+            {
+                var point = hits[index].point;
+                float cur_len = point.x * point.x + point.y * point.y;
+
+                // 如果当前长度小于最小长度
+                if (cur_len < min_len)
+                {
+                    min_len = cur_len;
+                    min_index = index;
+                }
+            }
+
+            line.SetPosition(i * 2, this.transform.position);
+            line.SetPosition(i * 2 + 1, hits[min_index].point);
+        }
+    }
+}
